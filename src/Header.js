992f4af6b28501b76cf03f68737f75bd25f5c7af
Jeff Connelly <jeff.connelly6@gmail.com>
2018-04-23 13:29:59 -0600

    Finished btn animation

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,7 @@ export default function HeaderBar(props) {
         <span className='heading-primary-main'>Outdoors</span>
         <span className='heading-primary-sub'>is where life happens</span>
         </h1>
-        <a href='#' className='btn btn-white'>Discover our tours</a>
+        <a href='#' className='btn btn-white btn-animated'>Discover our tours</a>
       </div>
     </header>
   );
