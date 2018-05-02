b70e1b4246efaa636343b00dd853af80ab5bff62
Jeff Connelly <jeff.connelly6@gmail.com>
2018-05-01 18:20:36 -0600

    Added test markup for grid

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import LogoWhite from './assets/logo-white.png';
 
 export default function HeaderBar(props) {
   return (
+    <section>
     <header className='header'>
       <div className='header__logo-box'>
         <img src={LogoWhite} alt='Logo' className='header__logo' />
@@ -16,5 +17,79 @@ export default function HeaderBar(props) {
         <a href='#' className='btn btn--white btn--animated'>Discover our tours</a>
       </div>
     </header>
+
+    <section className='grid-test'>
+      <div className='row'>
+        <div className='col-1-of-2'>
+        Col 1 of 2
+        </div>
+        <div className='col-1-of-2'>
+        Col 1 of 2
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-3'>
+        Col 1 of 3
+        </div>
+        <div className='col-1-of-3'>
+        Col 1 of 3
+        </div>
+        <div className='col-1-of-3'>
+        Col 1 of 3
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-3'>
+        Col 1 of 3
+        </div>
+        <div className='col-2-of-3'>
+        Col 2 of 3
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-2-of-4'>
+        Col 2 of 4
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-4'>
+        Col 1 of 4
+        </div>
+        <div className='col-3-of-4'>
+        Col 3 of 4
+        </div>
+      </div>
+
+      
+
+      
+
+    </section>
+    </section>
   );
 }
