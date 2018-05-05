e71223492e7aacf8c164e439b063a80593bf1e6d
Jeff Connelly <jeff.connelly6@gmail.com>
2018-05-05 10:37:43 -0600

    Finish about section pt. 1

--- src/About.js
+++ src/About.js
@@ -3,7 +3,19 @@ import React from 'react';
 export default function About(props) {
 return(
   <section class='section-about'>
+
+  <div className='u-center-text u-margin-bottom-8'>
     <h2 className='heading-secondary'>Exciting tours for adventurous people</h2>
+  </div>
+
+  <div className='row'>
+    <div className='col-1-of-2'>
+    Text Content
+    </div>
+    <div className='col-1-of-2'>
+    Image Composition
+    </div>
+  </div>
   </section>
   );
 }
